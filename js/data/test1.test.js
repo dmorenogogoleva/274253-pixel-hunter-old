d44c33e10d05f8b296d23f0e8bdc04796aa44942
Daria Moreno-Gogoleva <iwanttobealight@yandex.ru>
2017-10-11 03:13:10 +0300

    added countGamePoints function and test for it

--- js/data/test1.test.js
+++ js/data/test1.test.js
@@ -1,14 +1,89 @@
 import assert from 'assert';
+import countGamePoints from '../countGamePoints';
 
-const array = [1, 2, 3, 4, 5];
+const ALL_LIFES = 3;
 
-// const countGamePoints = (arr) => {
-//   return arr;
-// };
+const makeTestArray = (nameOfArray, answers, radipness, slowness) => {
+  nameOfArray = [];
 
-describe(`Count game points`, () => {
+  for (let i = 0; i < 10; i++) {
+    nameOfArray.push({
+      'id': i + 1,
+      'answer': answers,
+      'rapid': radipness,
+      'slow': slowness
+    });
+  }
+  return nameOfArray;
+};
 
-  it(`argument should be an array`, () => {
-    assert.isArray(array, `wtf`);
+const allAnswersNormalTimeArray = makeTestArray(`allAnswersNormalTimeArray`, true, false, false);
+const noAnswersRapidTimeArray = makeTestArray(`noAnswersRapidTimeArray`, false, true, false);
+const allAnswersSlowTimeArray = makeTestArray(`allAnswersSlowTimeArray`, true, false, true);
+const noAnswersSlowTimeArray = makeTestArray(`noAnswersSlowTimeArray`, false, false, true);
+
+const allAnswersNormalTimeAllLifesTest = (array, lifes) => {
+  describe(`Count game points`, () => {
+    it(`first argument should be an array`, () => {
+      assert((Array.isArray(array)));
+    });
+    it(`user must answered at ten questions or more (now its ${array.length} answers`, () => {
+      assert(array.length >= 10);
+    });
+    it(`user answered at all questions with normal speed and saved all lives`, () => {
+      assert.equal(countGamePoints(array, lifes), 1150);
+    });
   });
-});
+};
+
+allAnswersNormalTimeAllLifesTest(allAnswersNormalTimeArray, ALL_LIFES);
+
+
+const noAnswersRapidTimeAllLifesTest = (array, lifes) => {
+  describe(`Count game points`, () => {
+    it(`first argument should be an array`, () => {
+      assert((Array.isArray(array)));
+    });
+    it(`user must answered at ten questions or more (now its ${array.length} answers`, () => {
+      assert(array.length >= 10);
+    });
+    it(`user failed all questions with rapid speed and saved all lives`, () => {
+      assert.equal(countGamePoints(array, lifes), 500);
+    });
+  });
+};
+
+noAnswersRapidTimeAllLifesTest(noAnswersRapidTimeArray, ALL_LIFES - ALL_LIFES);
+
+const allAnswersSlowTimeArrayAllLifesTest = (array, lifes) => {
+  describe(`Count game points`, () => {
+    it(`first argument should be an array`, () => {
+      assert((Array.isArray(array)));
+    });
+    it(`user must answered at ten questions or more (now its ${array.length} answers`, () => {
+      assert(array.length >= 10);
+    });
+    it(`user answered at all questions with slow speed and saved all lives`, () => {
+      assert.equal(countGamePoints(array, lifes), 650);
+    });
+  });
+};
+
+allAnswersSlowTimeArrayAllLifesTest(allAnswersSlowTimeArray, ALL_LIFES);
+
+
+const noAnswersSlowTimeNoLifesTest = (array, lifes) => {
+  describe(`Count game points`, () => {
+    it(`first argument should be an array`, () => {
+      assert((Array.isArray(array)));
+    });
+    it(`user must answered at ten questions or more (now its ${array.length} answers`, () => {
+      assert(array.length >= 10);
+    });
+    it(`user failed all questions with slow speed and saved no lives`, () => {
+      assert.equal(countGamePoints(array, lifes), -500);
+    });
+  });
+};
+
+noAnswersSlowTimeNoLifesTest(noAnswersSlowTimeArray, ALL_LIFES - ALL_LIFES);
